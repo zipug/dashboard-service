0b9a94a0195723319f56e7d80e2890f39771896f
gedjerone <faxtro@icloud.com>
2025-01-17 07:06:25 +0300

    fix: Fixed verification endpoint

--- internal/presentation/http/chi/run.go
+++ internal/presentation/http/chi/run.go
@@ -81,13 +81,13 @@ func NewHttpServer(app *application.DashboardService) *HttpServer {
 				r.Get("/{id}", getuserbyid.GetUserById(app, log))
 				r.Get("/all", getallusers.GetAllUsers(app, log))
 				r.Get("/me", getuserinfo.GetUserInfo(app, log, auth))
-				r.Get("/send-code/{target}", sendotp.SendOTP(app, log, auth))
+				r.Post("/send-code/{target}", sendotp.SendOTP(app, log, auth))
 			})
 
 			r.Group(func(r chi.Router) {
 				r.Post("/register", register.RegisterUser(app, log, auth, cfg.AccessTokenExpiration))
 				r.Post("/login", login.LoginUser(app, log, auth, cfg.AccessTokenExpiration))
-				r.Get("/send-code/{target}", sendotp.SendOTP(app, log, auth))
+				r.Post("/send-code/{target}", sendotp.SendOTP(app, log, auth))
 			})
 		})
 	})
